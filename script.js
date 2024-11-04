350c2dfda95b2fa28e55409d88d99073cd70ac28
jeremykola <j.kolasack@gmail.com>
2024-11-04 14:32:26 +0100

    Simplify rock paper scissor

--- script.js
+++ script.js
@@ -4,17 +4,15 @@
 
 
 
-function random (number) {
-return Math.floor(Math.random()*number);
 
-}
-console.log(random(3));
 
 function computer() {
-let choice = random(3);
+    let choice = Math.floor(Math.random()*3);
+
     if (choice === 0) {return "rock"}
     else if (choice === 1) {return "paper"}
     else {return "scissor"}
     
     }
     console.log(computer());
+
